c7fd86392ed3528f6537b5cf09c4535d35b13c4d
Vinod Sharma <dev.vinod@outlook.com>
2025-03-12 21:29:15 +0530

    Initial
    Initial structure commit

---
+++ src/main/java/in/vinodsharma/dynaroute/DynarouteApplication.java
@@ -0,0 +1,13 @@
+package in.vinodsharma.dynaroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynarouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynarouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/vinodsharma/dynaroute/DynarouteApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vinodsharma.dynaroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynarouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
